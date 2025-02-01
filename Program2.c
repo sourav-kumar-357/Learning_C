8198225a3ff3dd942fa6a58f67849cc8979cbe1b
Sourav Kumar <xsouravkumar357@gmail.com>
2025-02-01 12:43:00 +0530

    updated the program 2

---
+++ Program2.c
@@ -0,0 +1,11 @@
+// Taking input testing in C language
+
+#include <stdio.h>
+
+void main ()
+{
+    char name[50];
+    printf("Please enter your name here : ");
+    scanf("%s", &name);
+    printf("Good morning %s", name);
+}
